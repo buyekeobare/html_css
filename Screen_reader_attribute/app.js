fadcfe32a5e834b7020181fe5e5dfcc2bbd08d31
buyekeobare <buyekeobare@gmail.com>
2023-11-27 16:07:55 +0300

    Updated files

---
+++ Screen_reader_attribute/app.js
@@ -0,0 +1,17 @@
+/**
+ * When I click the menu trigger,
+ * the menu appears
+ * 
+ */
+
+  
+ 
+ /**
+  * When I click the menu trigger again,
+  * the menu disappears
+  */
+
+ /**
+  * When I open the menu focus goes,
+  * to the first item in the menu
+  */
